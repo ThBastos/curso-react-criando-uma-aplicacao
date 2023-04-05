9f8d989f101621863addf460a83f69ebf1e87ce8
Thalles Bastos <thbastos.web@gmail.com>
2023-04-05 18:11:03 -0300

    Aula 01 atualizada

--- 01/src/App.js
+++ 01/src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
+import { Teste } from './components/Teste';
 
 function App() {
-
+  
   const [abrirMenu, setAbrirMenu] = useState(false);
+
   return (
     <div className="App">
-      <h1>{abrirMenu ? 'Menu Aberto' : 'Menu Fechado'}</h1>
-      <p>Valor do estado abrirMenu: {abrirMenu.toString()}</p>
-      <button onClick={() => setAbrirMenu(!abrirMenu)}>Mudar estado do menu</button>
+        <h1>Mostre o valor de abrirMenu: {abrirMenu}</h1>
+        <h1>{abrirMenu ? 'verdadeiro' : 'falso'}</h1>
+        <button onClick={() => setAbrirMenu(!abrirMenu)}>Mudar</button>
+        <Teste nome="Lilica" />
+        <Teste nome="Katara" />
+        <Teste nome="Luna" />
     </div>
   );
 }
---
+++ 01/src/components/Teste.js
@@ -0,0 +1,6 @@
+
+export function Teste(props){
+  return (
+    <h1>Olá, sou um componente, minha propriedade nome é essa: {props.nome}</h1>
+  )
+}
